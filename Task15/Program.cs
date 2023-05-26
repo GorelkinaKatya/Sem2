cea1cc1076a695a78798a67eaad7f0c5672451b3
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-26 17:17:10 +0300

    done task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+if (n == 6)
+    Console.WriteLine("Этот день выходной");
+else if (n == 7)
+    Console.WriteLine("Этот день выходной");
+else
+    Console.WriteLine("Этот день НЕ выходной");
+
+    
