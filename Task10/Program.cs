912697261a0f91a485c6f17fc8241066a2a82458
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-05-26 16:57:21 +0300

    done task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите положительное трехзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+while (n < 100 || n > 999)
+{
+    Console.Write("Вы ошиблись! Введите положительное трехзначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine(n / 10 - n / 100 * 10);
